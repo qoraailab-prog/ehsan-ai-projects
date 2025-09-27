8658cab442158d1d8971db84c72f72d06f8eb57e
qoraailab-prog <qora.ai.lab@gmail.com>
2025-09-27 05:42:07 +0330

    Create day1.py
    This repository contains my personal journey of learning Python and Artificial Intelligence step by step.  
    It includes daily exercises, mini-projects, and freelancing-oriented projects.  
    The goal is to build a strong foundation in programming while creating practical projects that can be showcased to clients and employers.

---
+++ day1/day1.py
@@ -0,0 +1,8 @@
+name = "Ehsan"
+age = 38
+city = "UAE"
+height = 178
+likes_ai = True
+favorite_color = "blue and mustard"
+
+print(f"My name is {name}, I am {age} years old, I live in {city}, my height is {height} cm, do I like AI? {likes_ai}, and my favorite color is {favorite_color}.")
